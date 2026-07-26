1a8c31df226a67570ee4d355b31415a47ca9fad9
zzjwillberich <2918773364@qq.com>
2026-07-26 17:24:56 +0800

    app: apply --log-level to logger on startup

--- app/receiver/main.cpp
+++ app/receiver/main.cpp
@@ -1,8 +1,17 @@
+#include "common/Config.h"
 #include "common/Logger.h"
 
 int main(int argc, char** argv) {
-    (void)argc;
-    (void)argv;
+    Config config;
+    if (!config.parse(argc, argv)) {
+        return 1;
+    }
+
+    Logger::instance().setLevel(parseLogLevel(config.get("log-level", "info")));
+
     LOG_INFO("receiver", "receiver starting");
+    LOG_DEBUG("receiver", "log-level=%s listen=%s",
+              config.get("log-level", "info").c_str(),
+              config.get("listen", "0.0.0.0:9000").c_str());
     return 0;
 }
--- app/sender/main.cpp
+++ app/sender/main.cpp
@@ -1,8 +1,17 @@
+#include "common/Config.h"
 #include "common/Logger.h"
 
 int main(int argc, char** argv) {
-    (void)argc;
-    (void)argv;
+    Config config;
+    if (!config.parse(argc, argv)) {
+        return 1;
+    }
+
+    Logger::instance().setLevel(parseLogLevel(config.get("log-level", "info")));
+
     LOG_INFO("sender", "sender starting");
+    LOG_DEBUG("sender", "log-level=%s target=%s",
+              config.get("log-level", "info").c_str(),
+              config.get("target", "127.0.0.1:9000").c_str());
     return 0;
 }
